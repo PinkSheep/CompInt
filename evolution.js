6f1a7c01902f1ad1757091669c900ce2bee0e560
unknown <t413564@W01B321S.UBSPROD.MSAD.UBS.NET>
2015-02-24 15:22:15 +0100

    updated evolution

--- evolution.js
+++ evolution.js
@@ -1,15 +1,31 @@
 "use strict";
 var Evolution = function(binary){
-	this.h = null;
+	this.h;
 	this.d = null;
 	this.f = null;
 	this.g = null;
 	this.select = false;
 	this.binary = binary;
 };
+
+Evolution.prototype.iterate = function(arr){
+	if(!arr){
+		var array = this.generateArray();
+		array = this.evaluate(array);
+	} else{
+		var array = arr;
+	}
+	
+	array = this.selection(array);
+	array = this.recombination(array);
+	array = this.mutation(array);
+	array = this.evaluate(array);
+	return array;
+
+}
 Evolution.prototype.toReal = function(){
-	this.d = parseInt(this.binary.substring(0,4),2);
-	this.h = parseInt(this.binary.substring(4,8),2);
+	this.d = parseInt(this.binary.substring(0,5),2);
+	this.h = parseInt(this.binary.substring(5,10),2);
 }
 Evolution.prototype.evaluate = function(array){
 	array.forEach(function(value,index){
@@ -33,21 +49,21 @@ Evolution.prototype.generateArray = function(){
 
 function returnBinary(){
 	var arr = new Array();
-	for(var i=0;i<8;i++){
+	for(var i=0;i<10;i++){
 		arr.push(Math.floor(Math.random()*2)); 
 	}
-	return arr.join();
+	return arr.join('');
 }
 
 Evolution.prototype.selection = function(array){
 	var rangArray = new Array();
 	array.forEach(function(value,index){
 		if (value.select){
-			rangArray.push(value.f);
+			rangArray.push(value);
 		}
 	});
 
-	selArray = rangArray.sort(CompareForSort);
+	var selArray = rangArray.sort(CompareForSort);
 	var total = calculateTotalRang(selArray.length);
 	var newIndividuals = new Array();
 	for (var i = 0;i<30;i++){
@@ -59,33 +75,37 @@ Evolution.prototype.selection = function(array){
 }
 
 Evolution.prototype.recombination = function(array){
-	var singlepoint = Math.floor(Math.random() * 9 );
-	var first = Math.floor(Math.random() * (array.length + 1 - 0));
-	var second = Math.floor(Math.random() * (array.length + 1 - 0));
+	var singlepoint = Math.floor(Math.random() * 11 );
+	var first = Math.floor(Math.random() * (array.length + 1 ));
+	var second = Math.floor(Math.random() * (array.length + 1 ));
 	var substring1 = array[first].binary.substring(0,singlepoint);
 	var substring2 = array[second].binary.substring(0,singlepoint);
-	array[first].binary = substring2 + array[first].binary.substring(singlepoint,8);
-	array[second].binary = substring1 + array[second].binary.substring(singlepoint,8);
+	array[first].binary = substring2 + array[first].binary.substring(singlepoint,10);
+	array[second].binary = substring1 + array[second].binary.substring(singlepoint,10);
 	return array;
 }
 
 Evolution.prototype.mutation = function(array){
-	array.forEach(function(value,index){
-		var stringArr = value.binary.split('');
+	for (var index = 0;index<array.length;index++){
+		var stringArr = array[index].binary.split('');
 		for(var i = 0;i<stringArr.length;i++){
 			if (Math.random()< 0.1){
 				stringArr[i] == "1"?stringArr[i]="0":stringArr[i]="1";
 			}
 		}
-	});
+		array[index].binary = stringArr.join('');
+	}
 	return array;
 }
 
 function rankBasedSelection(length,total,value){
-	for(var i = 0;i<array.length;i++){
-		if (value<(i+1)/total){
+	var teilSum = 0;
+	for(var i = 0;i<length;i++){
+		teilSum += (i+1)/total;
+		if (value<teilSum){
 			return i;
 		}
+
 	}
 }
 function calculateTotalRang(value){
@@ -105,5 +125,3 @@ function CompareForSort(first, second)
     else
         return 1; 
 }
-
-//Recombination TBD
