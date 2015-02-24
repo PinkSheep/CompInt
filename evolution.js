d033faaf3957842fca7d27dd27424957c03ed3f8
PinkSheep <oli.fav@bluewin.ch>
2015-02-24 21:44:57 +0100

    refactoring, added chart

--- evolution.js
+++ evolution.js
@@ -1,104 +1,140 @@
-"use strict";
-var Evolution = function(binary){
-	this.h;
-	this.d = null;
-	this.f = null;
-	this.g = null;
-	this.select = false;
-	this.binary = binary;
+	"use strict";
+var Evolution = function(){
+	this.besth = null;
+	this.bestd = null;
+	this.best = null;
+	this.individuals = new Array();
 };
 
-Evolution.prototype.iterate = function(arr){
-	if(!arr){
-		var array = this.generateArray();
+Evolution.prototype.iterate = function(){
+	var chartData = new Array();
+	var labelData = new Array();
+	for (var i = 0;i<100;i++){
+		if(i==0){
+			var array = this.generateArray();
+			array = this.evaluate(array);
+		}
+		
+		array = this.selection(array);
+		for (var i2 = 0;i2<5;i2++){
+			array=this.recombination(array);
+		}
+		array = this.mutation(array);
 		array = this.evaluate(array);
-	} else{
-		var array = arr;
+		var bestIndex = this.selectBest(array);
+		this.best = array[bestIndex].f;
+		this.h = array[bestIndex].h;
+		this.d = array[bestIndex].d;
+		console.log("Best:"+this.best+" h:"+this.h+" d:"+this.d);
+		chartData.push(this.best);
+		labelData.push(i);
 	}
-	
-	array = this.selection(array);
-	array = this.recombination(array);
-	array = this.mutation(array);
-	array = this.evaluate(array);
+	var ctx = document.getElementById("myChart").getContext("2d");
+	var data = {
+    labels: labelData,
+    datasets: [
+        {
+            label: "dataset",
+            fillColor: "rgba(220,220,220,0.2)",
+            strokeColor: "rgba(220,220,220,1)",
+            pointColor: "rgba(220,220,220,1)",
+            pointStrokeColor: "#fff",
+            pointHighlightFill: "#fff",
+            pointHighlightStroke: "rgba(220,220,220,1)",
+            data: chartData
+        }
+    	]
+	};	
+	var myLineChart = new Chart(ctx).Line(data,options);
+
+
 	return array;
 
 }
-Evolution.prototype.toReal = function(){
-	this.d = parseInt(this.binary.substring(0,5),2);
-	this.h = parseInt(this.binary.substring(5,10),2);
-}
-Evolution.prototype.evaluate = function(array){
-	array.forEach(function(value,index){
-		value.toReal();
-		value.f = ((Math.PI*Math.pow(value.d,2))/2)+Math.PI*value.d*value.h;
-		value.g = (Math.PI*Math.pow(value.d,2)*value.h)/4;
-		if (value.g >= 300){
-			value.select = true;
+
+Evolution.prototype.selectBest = function(){
+	var bestNum = null;
+	var indexVal = null;
+	for(var i = 0;i<this.individuals.length;i++){
+		if(bestNum==null&&this.individuals[i].select){
+			bestNum = this.individuals[i].f;
+			indexVal = i;
+		} else {
+			if(this.individuals[i].f<bestNum&&this.individuals[i].select){
+				bestNum=this.individuals[i].f;
+				indexVal=i;
+			}
+
 		}
-	});
-	return array;
-	
+	}
+	this.best=this.individuals[bestIndex].f;
+	this.besth=this.individuals[bestIndex].h;
+	this.bestd=this.individuals[bestIndex].d;
 }
+
 Evolution.prototype.generateArray = function(){
 	var array = new Array();
 	for(var i = 0;i<30;i++){
-		array[i] = new Evolution(returnBinary());
+		array[i] = new Individual(this.returnBinary());
 	};
-	return array;
+	this.individuals = array;
 }
 
-function returnBinary(){
+Evolution.prototype.returnBinary = function(){
 	var arr = new Array();
 	for(var i=0;i<10;i++){
 		arr.push(Math.floor(Math.random()*2)); 
 	}
 	return arr.join('');
 }
-
-Evolution.prototype.selection = function(array){
+Evolution.prototype.evaluate = function(){
+	this.individuals.forEach(function(value,index){
+		value.evaluate();
+	});
+}
+Evolution.prototype.selection = function(){
 	var rangArray = new Array();
-	array.forEach(function(value,index){
+	this.individuals.forEach(function(value,index){
 		if (value.select){
 			rangArray.push(value);
 		}
 	});
 
-	var selArray = rangArray.sort(CompareForSort);
-	var total = calculateTotalRang(selArray.length);
+	var selArray = rangArray.sort(this.compareForSort);
+	var total = this.calculateTotalRang(selArray.length);
 	var newIndividuals = new Array();
 	for (var i = 0;i<30;i++){
-		var ind = rankBasedSelection(selArray.length,total,Math.random());	
-		newIndividuals.push(new Evolution(selArray[ind].binary));
+		var ind = this.rankBasedSelection(selArray.length,total,Math.random());	
+		newIndividuals.push(new Individual(selArray[ind].binary));
 	}
-	return newIndividuals;
+	this.individuals = newIndividuals;
 
 }
 
-Evolution.prototype.recombination = function(array){
+Evolution.prototype.recombination = function(){
+
 	var singlepoint = Math.floor(Math.random() * 11 );
-	var first = Math.floor(Math.random() * (array.length));
-	var second = Math.floor(Math.random() * (array.length));
-	var substring1 = array[first].binary.substring(0,singlepoint);
-	var substring2 = array[second].binary.substring(0,singlepoint);
-	array[first].binary = substring2 + array[first].binary.substring(singlepoint,10);
-	array[second].binary = substring1 + array[second].binary.substring(singlepoint,10);
-	return array;
+	var first = Math.floor(Math.random() * (this.individuals.length));
+	var second = Math.floor(Math.random() * (this.individuals.length));
+	var substring1 = this.individuals[first].binary.substring(0,singlepoint);
+	var substring2 = this.individuals[second].binary.substring(0,singlepoint);
+	this.individuals[first].binary = substring2 + this.individuals[first].binary.substring(singlepoint,10);
+	this.individuals[second].binary = substring1 + this.individuals[second].binary.substring(singlepoint,10);
 }
 
-Evolution.prototype.mutation = function(array){
-	for (var index = 0;index<array.length;index++){
-		var stringArr = array[index].binary.split('');
+Evolution.prototype.mutation = function(){
+	for (var index = 0;index<this.individuals.length;index++){
+		var stringArr = this.individuals[index].binary.split('');
 		for(var i = 0;i<stringArr.length;i++){
-			if (Math.random()< 0.1){
+			if (Math.random()< 0.01){
 				stringArr[i] == "1"?stringArr[i]="0":stringArr[i]="1";
 			}
 		}
-		array[index].binary = stringArr.join('');
+		this.individuals[index].binary = stringArr.join('');
 	}
-	return array;
 }
 
-function rankBasedSelection(length,total,value){
+Evolution.prototype.rankBasedSelection = function(length,total,value){
 	var teilSum = 0;
 	for(var i = 0;i<length;i++){
 		teilSum += (i+1)/total;
@@ -108,7 +144,7 @@ function rankBasedSelection(length,total,value){
 
 	}
 }
-function calculateTotalRang(value){
+Evolution.prototype.calculateTotalRang = function(value){
 	var tot = 0;
 	for (var i = 0;i<value;i++){
 		tot += i + 1;
@@ -116,12 +152,61 @@ function calculateTotalRang(value){
 	return tot;
 }
 
-function CompareForSort(first, second)
+Evolution.prototype.compareForSort = function(first, second)
 {
     if (first.f == second.f)
         return 0;
     if (first.f < second.f)
-        return -1;
+        return 1;
     else
-        return 1; 
+        return -1; 
 }
+
+var options = {
+
+    ///Boolean - Whether grid lines are shown across the chart
+    scaleShowGridLines : true,
+
+    //String - Colour of the grid lines
+    scaleGridLineColor : "rgba(0,0,0,.05)",
+
+    //Number - Width of the grid lines
+    scaleGridLineWidth : 20,
+
+    //Boolean - Whether to show horizontal lines (except X axis)
+    scaleShowHorizontalLines: false,
+
+    //Boolean - Whether to show vertical lines (except Y axis)
+    scaleShowVerticalLines: false,
+
+    //Boolean - Whether the line is curved between points
+    bezierCurve : true,
+
+    //Number - Tension of the bezier curve between points
+    bezierCurveTension : 0.4,
+
+    //Boolean - Whether to show a dot for each point
+    pointDot : false,
+
+    //Number - Radius of each point dot in pixels
+    pointDotRadius : 4,
+
+    //Number - Pixel width of point dot stroke
+    pointDotStrokeWidth : 1,
+
+    //Number - amount extra to add to the radius to cater for hit detection outside the drawn point
+    pointHitDetectionRadius : 20,
+
+    //Boolean - Whether to show a stroke for datasets
+    datasetStroke : false,
+
+    //Number - Pixel width of dataset stroke
+    datasetStrokeWidth : 2,
+
+    //Boolean - Whether to fill the dataset with a colour
+    datasetFill : true,
+
+    //String - A legend template
+    legendTemplate : "<ul class=\"<%=name.toLowerCase()%>-legend\"><% for (var i=0; i<datasets.length; i++){%><li><span style=\"background-color:<%=datasets[i].strokeColor%>\"></span><%if(datasets[i].label){%><%=datasets[i].label%><%}%></li><%}%></ul>"
+
+};
---
+++ individual.js
@@ -0,0 +1,28 @@
+	"use strict";
+var Individual = function(binary){
+	this.h = null;
+	this.d = null;
+	this.f = null;
+	this.g = null;
+	this.select = false;
+	this.binary = binary;
+	this.kappa = 15;
+	this.mutationrate = 0.01;
+};
+
+Individual.prototype.toReal = function(){
+	this.d = parseInt(this.binary.substring(0,5),2);
+	this.h = parseInt(this.binary.substring(5,10),2);
+}
+
+Individual.prototype.evaluate = function(){
+	this.toReal();
+	this.f = ((Math.PI*Math.pow(this.d,2))/2)+Math.PI*this.d*this.h;
+	this.g = (Math.PI*Math.pow(this.d,2)*this.h)/4;
+	if (this.g >= 300){
+		this.select = true;
+	}	
+}
+
+
+
